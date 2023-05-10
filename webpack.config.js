adfc379bb97ab5fa1d19f6a5ab274ef0ba2c4ab7
juandapps <juandapps@hotmail.com>
2023-05-10 06:33:36 -0500

    ✨Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: {
-    index: './src/index.js',
-  },
+  entry: './src/index.js',
   module: {
     rules: [
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
     ],
   },
+  devtool: 'inline-source-map',
   devServer: {
     static: './dist',
   },
@@ -25,8 +32,6 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-  },
-  optimization: {
-    runtimeChunk: 'single',
+    clean: true,
   },
 };
