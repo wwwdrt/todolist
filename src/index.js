3a5ba60a553330f11a2c5e53adf956090c522247
juandapps <juandapps@hotmail.com>
2023-05-12 13:34:00 -0500

    ➕Setup component communication

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
-/* eslint-disable */
-
 import Data from './components/Data.js';
 import Create from './components/Create.js';
 import Read from './components/Read.js';
 import Update from './components/Update.js';
 import Destroy from './components/Destroy.js';
 
-new Data();
+const tasks = new Data();
+const read = new Read(tasks);
+const update = new Update(tasks, read);
+const create = new Create(tasks);
+// eslint-disable-next-line no-new
+new Destroy(tasks);
+
+tasks.loadTasks();
+read.setupEventListeners();
+update.load();
+create.setupFormEventListener();
