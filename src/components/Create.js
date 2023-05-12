cfcce053474dff54bc43bf127175215259c8b9c3
juandapps <juandapps@hotmail.com>
2023-05-12 13:35:05 -0500

    🛠️Update Create Component

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,6 +1,23 @@
-/* eslint-disable */
 class Create {
-  constructor() {
+  constructor(tasks) {
+    this.tasks = tasks;
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+  }
+
+  setupFormEventListener = () => {
+    const form = document.querySelector('#form');
+    form.addEventListener('submit', this.handleFormSubmit);
+  };
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    const description = input.value.trim();
+    if (description !== '') {
+      const index = this.tasks.tasks.length;
+      this.tasks.addTask({ description, completed: false, index });
+      input.value = '';
+    }
   }
 }
 
