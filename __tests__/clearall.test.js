95a47f2664d7de1c4cb6bdba5fe7277a91006f38
juandapps <juandapps@hotmail.com>
2023-05-19 21:45:16 -0500

    ➕Add clear all unit test

--- __tests__/clearall.test.js
+++ __tests__/clearall.test.js
@@ -1,1 +1,38 @@
-// I will be creating this part in the night, if you want to start, for me its okay.
+import Destroy from '../src/components/Destroy.js';
+
+describe('Destroy', () => {
+  let destroy;
+  let mockTasks;
+
+  beforeEach(() => {
+    // Arrange
+    document.body.innerHTML = `
+      <button id="clear"></button>
+      <button name="repeat-outline"></button>
+    `;
+
+    mockTasks = {
+      tasks: [],
+      updateTasksIndex: jest.fn(),
+      saveTasks: jest.fn(),
+      loadTasks: jest.fn(),
+    };
+
+    destroy = new Destroy(mockTasks);
+  });
+
+  afterEach(() => {
+    document.body.innerHTML = '';
+  });
+
+  test('should clear all completed tasks', () => {
+    // Act
+    destroy.clearAllCompletedTasks();
+
+    // Assert
+    expect(mockTasks.tasks).toEqual([]);
+    expect(mockTasks.updateTasksIndex).toHaveBeenCalled();
+    expect(mockTasks.saveTasks).toHaveBeenCalled();
+    expect(mockTasks.loadTasks).toHaveBeenCalled();
+  });
+});
